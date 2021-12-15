63708228b228b3b3f0862918eb6bd99a5301e9e5
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-15 13:19:55 +0000

    make function called getMovies

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_URL =
   'https://api.themoviedb.org/3/search/movie?api_key=0f0b8886571ef98e04b482be12a224d4&query"';
 
+// Get inital Movies
+getMovies(API_URL);
+
 async function getMovies(url) {
   const res = await fetch(url);
+  const data = await res.json();
 }
